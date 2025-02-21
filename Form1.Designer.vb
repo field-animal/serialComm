adeac6c5f8013ba0184da75b6487501a2f4374a0
kf <kf@mtouch.com>
2025-02-21 17:43:35 +0900

    초기 커밋.

---
+++ Form1.Designer.vb
@@ -0,0 +1,349 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim TreeNode1 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("$AVF02.0A*0C")
+        Dim TreeNode2 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("QVF", New System.Windows.Forms.TreeNode() {TreeNode1})
+        Dim TreeNode3 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("$AVSSJ-21L-2424-00001*19")
+        Dim TreeNode4 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("QV", New System.Windows.Forms.TreeNode() {TreeNode3})
+        Dim TreeNode5 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("$AS00000000D*56")
+        Dim TreeNode6 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("$AS2222222D*64")
+        Dim TreeNode7 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("$AS0222222U*77")
+        Dim TreeNode8 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("$AS3222222D*65")
+        Dim TreeNode9 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("QS", New System.Windows.Forms.TreeNode() {TreeNode5, TreeNode6, TreeNode7, TreeNode8})
+        Dim TreeNode10 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("$AH060*3F")
+        Dim TreeNode11 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("QH", New System.Windows.Forms.TreeNode() {TreeNode10})
+        Dim TreeNode12 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("$AT500*20")
+        Dim TreeNode13 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("$AT700*22")
+        Dim TreeNode14 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("QT", New System.Windows.Forms.TreeNode() {TreeNode12, TreeNode13})
+        Dim TreeNode15 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("$ACD*21")
+        Dim TreeNode16 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("$ACF*21")
+        Dim TreeNode17 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("QC", New System.Windows.Forms.TreeNode() {TreeNode15, TreeNode16})
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.tb_sendMessage = New System.Windows.Forms.TextBox()
+        Me.btn_DataSend = New System.Windows.Forms.Button()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.tsLabelPort = New System.Windows.Forms.ToolStripLabel()
+        Me.tsSplitbtnPort = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
+        Me.ToolStripDropDownButton1 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.tsBtnConnect = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripContainer3 = New System.Windows.Forms.ToolStripContainer()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer2.Panel1.SuspendLayout()
+        Me.SplitContainer2.Panel2.SuspendLayout()
+        Me.SplitContainer2.SuspendLayout()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
+        Me.ToolStripContainer3.ContentPanel.SuspendLayout()
+        Me.ToolStripContainer3.TopToolStripPanel.SuspendLayout()
+        Me.ToolStripContainer3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.ListBox1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.SplitContainer2)
+        Me.SplitContainer1.Size = New System.Drawing.Size(1866, 1076)
+        Me.SplitContainer1.SplitterDistance = 1052
+        Me.SplitContainer1.SplitterWidth = 10
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'ListBox1
+        '
+        Me.ListBox1.ColumnWidth = 600
+        Me.ListBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+        Me.ListBox1.Font = New System.Drawing.Font("굴림", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.HorizontalScrollbar = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(0, 0)
+        Me.ListBox1.MultiColumn = True
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.ScrollAlwaysVisible = True
+        Me.ListBox1.Size = New System.Drawing.Size(1050, 1074)
+        Me.ListBox1.TabIndex = 0
+        '
+        'SplitContainer2
+        '
+        Me.SplitContainer2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer2.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer2.Name = "SplitContainer2"
+        Me.SplitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer2.Panel1
+        '
+        Me.SplitContainer2.Panel1.Controls.Add(Me.TreeView1)
+        '
+        'SplitContainer2.Panel2
+        '
+        Me.SplitContainer2.Panel2.Controls.Add(Me.FlowLayoutPanel1)
+        Me.SplitContainer2.Size = New System.Drawing.Size(802, 1074)
+        Me.SplitContainer2.SplitterDistance = 874
+        Me.SplitContainer2.TabIndex = 0
+        '
+        'TreeView1
+        '
+        Me.TreeView1.CheckBoxes = True
+        Me.TreeView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TreeView1.Font = New System.Drawing.Font("굴림", 12.0!)
+        Me.TreeView1.HideSelection = False
+        Me.TreeView1.ItemHeight = 30
+        Me.TreeView1.LabelEdit = True
+        Me.TreeView1.Location = New System.Drawing.Point(0, 0)
+        Me.TreeView1.Name = "TreeView1"
+        TreeNode1.Name = "노드1"
+        TreeNode1.Text = "$AVF02.0A*0C"
+        TreeNode2.Name = "노드0"
+        TreeNode2.Text = "QVF"
+        TreeNode3.Name = "AV"
+        TreeNode3.Text = "$AVSSJ-21L-2424-00001*19"
+        TreeNode4.Name = "cmdQV"
+        TreeNode4.Text = "QV"
+        TreeNode5.Name = "노드5"
+        TreeNode5.Text = "$AS00000000D*56"
+        TreeNode6.Name = "노드6"
+        TreeNode6.Text = "$AS2222222D*64"
+        TreeNode7.Name = "노드7"
+        TreeNode7.Text = "$AS0222222U*77"
+        TreeNode8.Name = "노드10"
+        TreeNode8.Text = "$AS3222222D*65"
+        TreeNode9.Name = "cmdQS"
+        TreeNode9.Text = "QS"
+        TreeNode10.Name = "노드11"
+        TreeNode10.Text = "$AH060*3F"
+        TreeNode11.Name = "노드2"
+        TreeNode11.Text = "QH"
+        TreeNode12.Name = "노드12"
+        TreeNode12.Text = "$AT500*20"
+        TreeNode13.Name = "노드13"
+        TreeNode13.Text = "$AT700*22"
+        TreeNode14.Name = "노드3"
+        TreeNode14.Text = "QT"
+        TreeNode15.Name = "노드14"
+        TreeNode15.Text = "$ACD*21"
+        TreeNode16.Name = "노드15"
+        TreeNode16.Text = "$ACF*21"
+        TreeNode17.Name = "노드8"
+        TreeNode17.Text = "QC"
+        Me.TreeView1.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode2, TreeNode4, TreeNode9, TreeNode11, TreeNode14, TreeNode17})
+        Me.TreeView1.Size = New System.Drawing.Size(802, 874)
+        Me.TreeView1.TabIndex = 0
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.AutoScroll = True
+        Me.FlowLayoutPanel1.AutoSize = True
+        Me.FlowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.FlowLayoutPanel1.Controls.Add(Me.tb_sendMessage)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btn_DataSend)
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(802, 196)
+        Me.FlowLayoutPanel1.TabIndex = 0
+        '
+        'tb_sendMessage
+        '
+        Me.tb_sendMessage.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tb_sendMessage.Location = New System.Drawing.Point(3, 3)
+        Me.tb_sendMessage.Name = "tb_sendMessage"
+        Me.tb_sendMessage.Size = New System.Drawing.Size(672, 28)
+        Me.tb_sendMessage.TabIndex = 0
+        '
+        'btn_DataSend
+        '
+        Me.btn_DataSend.Dock = System.Windows.Forms.DockStyle.Right
+        Me.btn_DataSend.Location = New System.Drawing.Point(681, 3)
+        Me.btn_DataSend.MinimumSize = New System.Drawing.Size(100, 60)
+        Me.btn_DataSend.Name = "btn_DataSend"
+        Me.btn_DataSend.Size = New System.Drawing.Size(109, 60)
+        Me.btn_DataSend.TabIndex = 1
+        Me.btn_DataSend.Text = "Send"
+        Me.btn_DataSend.UseVisualStyleBackColor = True
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Dock = System.Windows.Forms.DockStyle.None
+        Me.ToolStrip1.GripMargin = New System.Windows.Forms.Padding(10)
+        Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsLabelPort, Me.tsSplitbtnPort, Me.ToolStripSeparator2, Me.ToolStripLabel1, Me.ToolStripDropDownButton1, Me.ToolStripSeparator1, Me.tsBtnConnect})
+        Me.ToolStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Flow
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Margin = New System.Windows.Forms.Padding(0, 2, 0, 2)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Padding = New System.Windows.Forms.Padding(10)
+        Me.ToolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System
+        Me.ToolStrip1.Size = New System.Drawing.Size(1866, 54)
+        Me.ToolStrip1.Stretch = True
+        Me.ToolStrip1.TabIndex = 2
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'tsLabelPort
+        '
+        Me.tsLabelPort.Name = "tsLabelPort"
+        Me.tsLabelPort.Size = New System.Drawing.Size(45, 25)
+        Me.tsLabelPort.Text = "Port"
+        '
+        'tsSplitbtnPort
+        '
+        Me.tsSplitbtnPort.AutoSize = False
+        Me.tsSplitbtnPort.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.tsSplitbtnPort.Image = CType(resources.GetObject("tsSplitbtnPort.Image"), System.Drawing.Image)
+        Me.tsSplitbtnPort.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsSplitbtnPort.Margin = New System.Windows.Forms.Padding(0)
+        Me.tsSplitbtnPort.Name = "tsSplitbtnPort"
+        Me.tsSplitbtnPort.Size = New System.Drawing.Size(168, 29)
+        Me.tsSplitbtnPort.Text = "연결 포트 미지정"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(6, 23)
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(70, 25)
+        Me.ToolStripLabel1.Text = "bit rate"
+        '
+        'ToolStripDropDownButton1
+        '
+        Me.ToolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripDropDownButton1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem2})
+        Me.ToolStripDropDownButton1.Image = CType(resources.GetObject("ToolStripDropDownButton1.Image"), System.Drawing.Image)
+        Me.ToolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripDropDownButton1.Name = "ToolStripDropDownButton1"
+        Me.ToolStripDropDownButton1.Size = New System.Drawing.Size(84, 29)
+        Me.ToolStripDropDownButton1.Text = "미지정"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(174, 34)
+        Me.ToolStripMenuItem2.Text = "115200"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 23)
+        '
+        'tsBtnConnect
+        '
+        Me.tsBtnConnect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.tsBtnConnect.Image = CType(resources.GetObject("tsBtnConnect.Image"), System.Drawing.Image)
+        Me.tsBtnConnect.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsBtnConnect.Margin = New System.Windows.Forms.Padding(10, 2, 10, 3)
+        Me.tsBtnConnect.Name = "tsBtnConnect"
+        Me.tsBtnConnect.Size = New System.Drawing.Size(83, 29)
+        Me.tsBtnConnect.Text = "Connect"
+        '
+        'ToolStripContainer3
+        '
+        '
+        'ToolStripContainer3.ContentPanel
+        '
+        Me.ToolStripContainer3.ContentPanel.Controls.Add(Me.SplitContainer1)
+        Me.ToolStripContainer3.ContentPanel.Size = New System.Drawing.Size(1866, 1076)
+        Me.ToolStripContainer3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ToolStripContainer3.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStripContainer3.Name = "ToolStripContainer3"
+        Me.ToolStripContainer3.Size = New System.Drawing.Size(1866, 1130)
+        Me.ToolStripContainer3.TabIndex = 4
+        Me.ToolStripContainer3.Text = "ToolStripContainer3"
+        '
+        'ToolStripContainer3.TopToolStripPanel
+        '
+        Me.ToolStripContainer3.TopToolStripPanel.Controls.Add(Me.ToolStrip1)
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1866, 1130)
+        Me.Controls.Add(Me.ToolStripContainer3)
+        Me.Name = "frmMain"
+        Me.Text = "serial Automation Tool"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.SplitContainer2.Panel1.ResumeLayout(False)
+        Me.SplitContainer2.Panel2.ResumeLayout(False)
+        Me.SplitContainer2.Panel2.PerformLayout()
+        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer2.ResumeLayout(False)
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.PerformLayout()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ToolStripContainer3.ContentPanel.ResumeLayout(False)
+        Me.ToolStripContainer3.TopToolStripPanel.ResumeLayout(False)
+        Me.ToolStripContainer3.TopToolStripPanel.PerformLayout()
+        Me.ToolStripContainer3.ResumeLayout(False)
+        Me.ToolStripContainer3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents tsLabelPort As ToolStripLabel
+    Friend WithEvents tsSplitbtnPort As ToolStripDropDownButton
+    Friend WithEvents ToolStripLabel1 As ToolStripLabel
+    Friend WithEvents ToolStripDropDownButton1 As ToolStripDropDownButton
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents tsBtnConnect As ToolStripButton
+    Friend WithEvents ToolStripContainer3 As ToolStripContainer
+    Friend WithEvents SplitContainer2 As SplitContainer
+    Friend WithEvents tb_sendMessage As TextBox
+    Friend WithEvents btn_DataSend As Button
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents TreeView1 As TreeView
+End Class
---
+++ Form1.vb
@@ -0,0 +1,213 @@
+﻿Imports System.IO.Ports
+Imports System.Threading
+Imports System.Windows.Forms
+
+
+Public Class frmMain
+    Public returnStr As String = ""
+    Private WithEvents serialPort As SerialPort
+    Private receivingThread As Thread
+    Private keepReceiving As Boolean = True
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '첫 실행 시, 시리얼 포트 목록을 획득
+        GetSerialPortNames()
+
+        TreeView1.ExpandAll()
+    End Sub
+
+    'HW의 포트 구성요소를 획득하여 드랍다운 아이템에 추가
+    Public Sub GetSerialPortNames()
+        ' Show all available COM ports.
+        Dim indexNum As Integer = 0
+        tsSplitbtnPort.DropDownItems.Clear()
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+
+            tsSplitbtnPort.DropDownItems.Add(sp)
+            indexNum = +1
+        Next
+
+        If tsSplitbtnPort.DropDownItems.Count <= 0 Then
+            tsSplitbtnPort.Enabled = False
+        Else
+            tsSplitbtnPort.Enabled = True
+        End If
+    End Sub
+
+    '드랍다운 아이템에서 선택한 아이템을 컴포넌트의 텍스트로 적용
+    Private Sub tsSplitbtnPort_DropDownItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles tsSplitbtnPort.DropDownItemClicked
+        tsSplitbtnPort.Text = e.ClickedItem.Text
+    End Sub
+
+    Private Sub tsBtnConnect_Click(sender As Object, e As EventArgs) Handles tsBtnConnect.Click
+
+        Try
+            serialPort = New SerialPort(tsSplitbtnPort.Text, 115200, Parity.None, 8, StopBits.One)
+            serialPort.Open()
+
+            '정상적으로 연결되었는지 확인하기 위한 테스트 코드
+            'SendData(Now() & ": 시리얼 통신 시작" & vbCrLf)
+            'serialPort.ReadTimeout = 10000
+            keepReceiving = True
+            tsBtnConnect.Text = "disconnect"
+
+        Catch ex As TimeoutException
+            returnStr = "error : serial port read time out"
+            MsgBox(returnStr)
+        Catch ex As Exception
+            returnStr = "error : reject connect"
+            MsgBox(returnStr)
+        Finally
+            'If serialPort IsNot Nothing Then serialPort.Close()
+        End Try
+        Console.WriteLine(serialPort.ReceivedBytesThreshold)
+    End Sub
+
+    ' 데이터 수신을 위한 비동기 이벤트 핸들러
+    Public Sub serialPort_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles serialPort.DataReceived
+        '수신된 메시지 자동 응답 함수 실행
+        autoAnswer(serialPort.ReadExisting(), "$", "*", "<<")
+
+    End Sub
+
+    Private Function autoAnswer(ByVal receviedValue As String, startLetter As String, checksumLetter As String, msgDirect As String)
+        '수신 데이터에 복수의 데이터가 포함되어 있을 수 있으므로, 수신 데이터를 $로 구분하여 배열에 저장
+        Dim receivedDATAs() As String = Split(receviedValue, startLetter)
+        '수신 데이터 배열의 인덱스를 구분하기 위한 변수
+        Dim indexOfreceviedData As Integer = 0
+
+
+        For indexOfreceviedData = 0 To receivedDATAs.Count - 1
+            If receivedDATAs(indexOfreceviedData).Contains(checksumLetter) = True Then
+
+                '수신 데이터이므로 <<를 추가
+                addData(Now() & msgDirect & receivedDATAs(indexOfreceviedData), False)
+                Dim autoSendValue As String = vbNullString
+                Dim i As Integer = 0
+                '수신 데이터의 명령을 트리뷰에서 찾아서 자식 노드의 응답값으로 자동 송신
+                autoSendValue = vbNullString
+                Dim strSearchstring As String
+                '수신 데이터에서 체크섬 이후의 값을 삭제하여 검색어 생성.
+                strSearchstring = Microsoft.VisualBasic.Strings.Left(receivedDATAs(indexOfreceviedData), InStr(receivedDATAs(indexOfreceviedData), checksumLetter) - 1)
+
+                '트리 노드의 개수 만큼 for문 반복하여 각 노드의 정보 획득
+                For i = 0 To TreeView1.Nodes.Count - 1
+                    If TreeView1.Nodes.Item(i).Checked Then
+                        If TreeView1.Nodes.Item(i).Text = strSearchstring Then
+                            '자동 응답으로 전송하려는 응답값을 트리 노드에서 검색하여 존재할 경우 
+                            autoSendValue = TreeView1.Nodes.Item(i).Nodes.Item(0).Text
+                            SendData(autoSendValue)
+                            Exit For
+                            '자동 응답을 완료하지 못하면 false 반환
+                            Return False
+                        End If
+                    End If
+                    Console.WriteLine(i & strSearchstring)
+                Next
+            End If
+        Next
+        '자동 응답을 완료하면 true 반환
+        Return True
+    End Function
+
+
+    ' 데이터 송신
+    Public Function SendData(ByVal data As String) As Boolean
+        Console.WriteLine(Me.Text & "포트 열림 상태" & serialPort.IsOpen)
+
+        If serialPort IsNot Nothing AndAlso serialPort.IsOpen Then
+            Console.WriteLine("데이터 전송합니다." & data)
+            Try
+                serialPort.WriteLine(data)
+                addData(Now() & " >> " & data, True)
+                Console.WriteLine("송신: " & data)
+                Return True
+            Catch ex As Exception
+
+                Console.WriteLine("===============")
+                Console.WriteLine("송신 오류: " & ex.Message)
+                Console.WriteLine("오류 내용" & ex.GetBaseException.ToString)
+                Console.WriteLine("===============")
+                Return False
+            End Try
+        Else
+            Console.WriteLine("포트 안 열려있는뎅")
+            Return False
+        End If
+    End Function
+
+    'Data를 List Box에 추가
+    Public Sub addData(ByVal str_addDATA As String, isSendData As Boolean)
+        If ListBox1.InvokeRequired Then
+            ListBox1.Invoke(Sub() ListBox1.Items.Add(str_addDATA))
+        Else
+            ListBox1.Items.Add(str_addDATA)
+        End If
+    End Sub
+
+    ' 데이터 송신  버튼의 이벤트 핸들러
+    Private Sub Btn_DataSend_Click(sender As Object, e As EventArgs) Handles btn_DataSend.Click
+        Dim data As String = tb_sendMessage.Text
+        If SendData(data) Then
+            Console.WriteLine("송신 성공: " & data)
+        Else
+            Console.WriteLine("송신 실패")
+        End If
+    End Sub
+
+    ' 시리얼 포트 닫기
+    Public Sub ClosePort()
+        Try
+            keepReceiving = False
+            If serialPort IsNot Nothing AndAlso serialPort.IsOpen Then
+                serialPort.Close()
+                Console.WriteLine("시리얼 포트가 닫혔습니다.")
+            End If
+        Catch ex As Exception
+            Console.WriteLine("포트 닫기 오류: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ListBox1_DrawItem(sender As Object, e As DrawItemEventArgs) Handles ListBox1.DrawItem
+        If tsBtnConnect.Text = "Connect" Then
+            Exit Sub
+        Else
+            e.DrawBackground()
+            Dim brush As Brush = Brushes.Black
+            Dim direction As String = ListBox1.Items(e.Index).ToString
+            '수발신 데이터에 << 가 포함되어 있으면 수신, >>가 포함되어 있으면 송신으로 판단
+            If direction.IndexOf("<<") > 0 Then
+                direction = "<<"
+            Else
+                direction = ">>"
+            End If
+            ' 수발신 데이터에 따라 색상을 다르게 표시
+            If direction = "<<" Then
+                brush = Brushes.HotPink
+            Else
+                brush = Brushes.DarkGreen
+            End If
+
+            If e.Index >= 0 Then
+                e.Graphics.DrawString(ListBox1.Items(e.Index).ToString(), e.Font, brush, e.Bounds, StringFormat.GenericDefault)
+            End If
+        End If
+    End Sub
+
+    Private Sub TreeView1_NodeMouseDoubleClick(sender As Object, e As TreeNodeMouseClickEventArgs) Handles TreeView1.NodeMouseDoubleClick
+        tb_sendMessage.Text = e.Node.Text
+        SendData(tb_sendMessage.Text)
+    End Sub
+
+    Private Sub TreeView1_NodeMouseClick(sender As Object, e As TreeNodeMouseClickEventArgs) Handles TreeView1.NodeMouseClick
+        tb_sendMessage.Text = e.Node.Text
+    End Sub
+
+    Private Sub tb_sendMessage_KeyPress(sender As Object, e As KeyPressEventArgs) Handles tb_sendMessage.KeyPress
+        If e.KeyChar.Equals(Keys.Enter) Then SendData(tb_sendMessage.Text)
+    End Sub
+End Class
+
+
